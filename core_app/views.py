eed9cf0c7c885f5b7a70dc7ae4d0ddb7cd595f96
Vladimir Ionita <ionita.vladimir@gmail.com>
2020-07-01 09:32:52 -0500

    Save the search query for every search

--- core_app/views.py
+++ core_app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+
+def search(request):
+    search_query = request.POST.get('search')
+    models.Search.objects.create(search=search_query)
